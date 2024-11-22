a1232588a440a1fb8082f237662e70a9829e9fec
MrAmoges <s.sigma.top@gmail.com>
2024-11-22 11:12:51 +0500

    change settings to deploy

--- ForumAPI/settings.py
+++ ForumAPI/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'ForumAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR,
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
